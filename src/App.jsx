456170dc710f1a1485394d21065391b3ee6e6255
tonnymuchui <tonykanyingah@gmail.com>
2024-07-16 23:27:31 +0300

    Update HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Home } from './page/Home/Home/Home'
 import { NavBar } from './page/Home/Navbar/NavBar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
     <NavBar/>
+    <Home/>
     </>
   )
 }
